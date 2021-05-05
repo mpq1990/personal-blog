346076160eb75f762e25c4e27c7583f39f5ad37f
Majid Qureshi <mpq1990@gmail.com>
2021-05-05 11:11:31 +0200

    wip: add destroy

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,6 +38,14 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
+    end
+  end
+
   private
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
