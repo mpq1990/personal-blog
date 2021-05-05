3fe050e001543823304c7d289029dd01dd57beab
Majid Qureshi <mpq1990@gmail.com>
2021-05-05 16:18:15 +0200

    wip: add custom scopes for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,10 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def react
+    @react_portfolio_items = Portfolio.react
+  end
+
   def new
     @portfolio_item = Portfolio.new 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
+  get "react-items", to: "portfolios#react", as: "react_show"
   get 'about-me', to: "pages#about"
   get 'contact', to: "pages#contact"
 
