a0c4fb10fa4797030c1ea90b6e48017f10b5e86d
Majid Qureshi <mpq1990@gmail.com>
2021-05-05 17:37:33 +0200

    Data Management (#3)
    * wip: implement model validations
    
    * wip: add relationship bw topic and blog
    
    * wip: add custom scopes for Portfolio
    
    * wip: implement image defaults
    
    * wip: add concern for img placeholder
    
    * wip: migrate technology model
    
    * wip: add nested attributes for portfolio with technologies
    
    * feat: form config for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,8 +3,13 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def react
+    @react_portfolio_items = Portfolio.react
+  end
+
   def new
     @portfolio_item = Portfolio.new 
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -48,6 +53,6 @@ class PortfoliosController < ApplicationController
 
   private
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |i|
+  Topic.create!(
+    title: "Topic #{i}"
+  )
+end
+
+puts "3 topics created"
+
 10.times do |i|
   Blog.create!(
     title: "My Blog POst #{i}",
-    body: "here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+    body: "here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-9.times do |i|
+8.times do |i|
   Portfolio.create!(
     title: "Portfolio title #{i}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "Maecenas dui diam, tempor at velit sed, pulvinar lacinia nisl. Praesent iaculis arcu id eros ultrices lobortis. Cras sit amet justo blandit, maximus ipsum eu, lobortis mauris. Sed sit amet massa augue. Nulla sed dolor neque. Duis metus urna, scelerisque at pulvinar at, tristique in quam. Etiam vel ultrices justo.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |i|
+  Portfolio.create!(
+    title: "Portfolio title #{i}",
+    subtitle: "React",
     body: "Maecenas dui diam, tempor at velit sed, pulvinar lacinia nisl. Praesent iaculis arcu id eros ultrices lobortis. Cras sit amet justo blandit, maximus ipsum eu, lobortis mauris. Sed sit amet massa augue. Nulla sed dolor neque. Duis metus urna, scelerisque at pulvinar at, tristique in quam. Etiam vel ultrices justo.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/350x200"
@@ -27,3 +46,9 @@ puts "5 skills created"
 end
 
 puts "9 portfolio items created"
+
+3.times do |i| 
+  Portfolio.last.technologies.create!(name: "Technology #{i}")
+end
+
+puts "3 technologies created"
