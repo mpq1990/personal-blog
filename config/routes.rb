e44325033d6b3d0a4c0918352a6ee715ddadfd2a
Majid Qureshi <mpq1990@gmail.com>
2021-05-06 12:47:01 +0200

    chore: add user with devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
   get "react-items", to: "portfolios#react", as: "react_show"
