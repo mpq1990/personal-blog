75efb0dfeaabd81623dd3975156c62c52f11bad9
Majid Qureshi <mpq1990@gmail.com>
2021-05-06 18:01:55 +0200

    feat: add null object pattern for current user

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,12 @@
+
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+
+  def current_user 
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: "some name", first_name: "Guest", last_name:"User", email: "guest@example.de")
+  end
+end
