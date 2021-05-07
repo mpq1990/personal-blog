3106d34c66da2f776882068a14fa7f009bf62700
Majid Qureshi <mpq1990@gmail.com>
2021-05-07 12:55:31 +0200

    wip: add custom layouts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy toggle_status ]
-
+  layout "blog"
+  
   # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
