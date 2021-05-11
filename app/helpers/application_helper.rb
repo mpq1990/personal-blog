e7a0990e13ff09f1909786e08a6a0e006dbeb7fa
Majid Qureshi <mpq1990@gmail.com>
2021-05-11 18:25:03 +0200

    wip: update style for master layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
-      (link_to 'Login', new_user_session_path) +
-        + '<br>'.html_safe +
-        (link_to 'Register', new_user_registration_path)
+      (link_to 'Login', new_user_session_path, class: style) +
+        + ' '.html_safe +
+        (link_to 'Register', new_user_registration_path, class: style)
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     end
   end
 
