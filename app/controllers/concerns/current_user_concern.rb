42ca4bed511a80618f2d966b151ddf792d47ae46
Majid Qureshi <mpq1990@gmail.com>
2021-05-11 16:13:18 +0200

    feat: add authorisation engine

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -8,6 +8,12 @@ module CurrentUserConcern
   end
 
   def guest_user
-    OpenStruct.new(name: 'some name', first_name: 'Guest', last_name: 'User', email: 'guest@example.de')
+    # OpenStruct.new(name: 'some name', first_name: 'Guest', last_name: 'User', email: 'guest@example.de')
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest
   end
 end
