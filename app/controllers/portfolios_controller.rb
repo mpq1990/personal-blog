7b1f88ae7aab2290eca072f8ab9c0ec91b181d81
Majid Qureshi <mpq1990@gmail.com>
2021-05-15 22:05:42 +0200

    feat: add auth rules

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,8 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: %i[edit update show destroy]
   layout 'portfolio'
+  access all: %i[show index react], user: { except: %i[destroy new create update edit sort] }, site_admin: :all
+
   def index
     @portfolio_items = Portfolio.by_position
   end
