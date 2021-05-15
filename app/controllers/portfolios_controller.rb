7e35d410e3734aa8346d39f921913199af5aa7e5
Majid Qureshi <mpq1990@gmail.com>
2021-05-15 20:37:37 +0200

    feat: add porfolio sorting by position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: %i[edit update show destroy]
   layout 'portfolio'
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def react
