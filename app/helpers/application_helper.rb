480a74c0a37e82a4e643548cacab46d2b25d598f
Majid Qureshi <mpq1990@gmail.com>
2021-05-15 18:06:24 +0200

    refactor: nav links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,42 @@ module ApplicationHelper
   def copyright_generator
     MpqViewTool::Renderer.copyright 'Majid', 'All rights reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: about_me_path,
+        title: "About Me"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      },
+      {
+        url: blogs_path,
+        title: "Blogs"
+      },
+      {
+        url: portfolios_path,
+        title: "Portfolio"
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = +""
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+end
+
+def active? path
+  "active" if current_page? path
 end
