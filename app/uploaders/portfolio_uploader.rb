5932e0ff927fd1f7018bd2ad93c8fba6faed8b33
Majid Qureshi <mpq1990@gmail.com>
2021-05-16 13:15:49 +0200

    wip: add aws connnection to s3

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -6,7 +6,8 @@ class PortfolioUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :
+  storage :aws
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
