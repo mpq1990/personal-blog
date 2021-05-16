f9768d6fa9a9d639e175c535af09121d4590281b
Majid Qureshi <mpq1990@gmail.com>
2021-05-16 13:50:33 +0200

    feat: add complete upload feature

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,9 +6,7 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: ->(attrs) { attrs['name'].blank? }
 
-  include Placeholder
-
-  validates :title, :body, :main_image, :thumb_image, presence: true
+  validates :title, :body, presence: true
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -21,13 +19,6 @@ class Portfolio < ApplicationRecord
   scope :by_position, -> { order('position ASC') }
 
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
-    self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
-  end
 end
 
 #######
