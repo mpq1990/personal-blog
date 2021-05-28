ddfdbe19bb1972a863a17c5b1dec57d3bfda75b7
Majid Qureshi <mpq1990@gmail.com>
2021-05-28 21:15:46 +0200

    chore: action cable set up for cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_cable.allowed_request_origins = ['https://mpq1990-protfolio.herokuapp.com/', 'http://mpq1990-protfolio.herokuapp.com/']
+  config.action_cable.url = "wss://mpq1990-protfolio.herokuapp.com/cable"
 end
